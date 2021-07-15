e066d67a23f3c126075aaa47ffac857490d0370c
Ethan Shaw <ethanrs@vt.edu>
2021-07-15 03:04:57 -0400

    Add LICENSE (MIT) and improve include guards

--- AVFormatContextHolder.hpp
+++ AVFormatContextHolder.hpp
@@ -1,7 +1,30 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2021 Ethan Shaw
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #pragma once
 
-#ifndef AV_FORMAT_CONTEXT_HOLDER_H__
-#define AV_FORMAT_CONTEXT_HOLDER_H__
+#ifndef LONG_EXPOSER_AV_FORMAT_CONTEXT_HOLDER_HPP_INCLUDED
+#define LONG_EXPOSER_AV_FORMAT_CONTEXT_HOLDER_HPP_INCLUDED
 
 #include "AVPacketHolder.hpp"
 extern "C"
@@ -47,4 +70,4 @@ public:
     int ReadFrame(AVPacketHolder& frameHolder);
 };
 
-#endif // AV_FORMAT_CONTEXT_HOLDER_H__
+#endif // LONG_EXPOSER_AV_FORMAT_CONTEXT_HOLDER_HPP_INCLUDED
