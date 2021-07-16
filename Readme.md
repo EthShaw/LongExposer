ae6553176bfb361b5b44346acbe257cc7f8889a8
Ethan Shaw <ethanrs@vt.edu>
2021-07-15 23:31:38 -0400

    Add argument parsing, more modes, updated Readme
    - Added an --intensity argument and a --mode argument along with three
       new modes (in addition to the original "average" mode): brightest,
       sum, and sum-scaled. See updated Readme.md for details on these
       arguments and modes.
    - Added dependency on p-ranav/argparse and included credit in Readme.md

--- Readme.md
+++ Readme.md
@@ -1,5 +1,5 @@
 # Long Exposer
-This is program takes the average of all of the frames in a video. It essentially finds the average R, G, and B components for each pixel across all frames of a video and creates a single `ppm` image from the averages. (See the [Wikipedia article](https://en.wikipedia.org/wiki/Netpbm#PPM_example) on the PPM format for more information.) The program can be used to create things like a "silky waterfall" effect that you could get from a long exposure photo, hence the name.
+This is program uses the frames in a video to create a single image with long-exposure type effects (and some other interesting effects). In its default mode, it finds the average R, G, and B components for each pixel across all frames of the input video and creates a single `ppm` image from the averages. (See the [Wikipedia article](https://en.wikipedia.org/wiki/Netpbm#PPM_example) on the PPM format for more information.) The program can be used to create things like a "silky waterfall" effect that you could get from a long-exposure photo, hence the name.
 
 # License and thirdparty software
 
@@ -7,14 +7,16 @@ This software is licensed under the MIT License.
 
 This software makes use of libraries from the (amazing) <a href=http://ffmpeg.org>FFmpeg</a> project, which is a thirdparty library licensed under the <a href=http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html>LGPLv2.1</a> (with certain optional components under the GPL). See [http://ffmpeg.org/](http://ffmpeg.org/) for more details on the project.
 
+This program also makes use of the extremely helpful <a href="http://github.com/p-ranav/argparse">p-ranav/argparse (Link to Repository)</a>, an argument parser for modern C++ by Pranav Srinivas Kumar. (Licensed under the MIT license. See [thirdparty/argparser/argparser.hpp](thirdparty/argparser/argparser.hpp) for more information.)
+
 # Compiling the project
-The project can compiled on linux by running `./configure` and `cmake --build build`. It requires FFmpeg libraries to be installed. In theory, the project can be compiled on Windows, but you will have to supply your own FFmpeg binaries. If you intend to redistribute compiled binaries of this project, ensure compliance with the FFmpeg licensing. To ensure that the built binary is still licensed under the MIT license and not the LGPL or GPL, ensure that the project is dynamically linked to FFmpeg and that no GPL components are enabled in the FFmpeg build you are using. If you are only using the project for personal use and not redistributing it, this should not be an issue anyway. For more information about FFmpeg licensing, see [https://www.ffmpeg.org/legal.html](https://www.ffmpeg.org/legal.html). **Disclaimer: I am not a lawyer and this is not legal advice. Furthermore, I am not affiliated with the FFmpeg project in any way.**
+The project can compiled on linux systems by running `./configure` and `cmake --build build`. It requires FFmpeg libraries to be installed. In theory, the project can be compiled on Windows, but you will have to supply your own FFmpeg binaries. If you intend to redistribute compiled binaries of this project, ensure compliance with the FFmpeg licensing. To ensure that the built binary is still licensed under the MIT license and not the LGPL or GPL, you will need to ensure that the project is dynamically linked to FFmpeg and that no GPL components are enabled in the FFmpeg build you are using. If you are only using the project for personal use and not redistributing it, this should not be an issue anyway. For more information about FFmpeg licensing, see [https://www.ffmpeg.org/legal.html](https://www.ffmpeg.org/legal.html). **Disclaimer: I am not a lawyer and this is not legal advice. Furthermore, I am not affiliated with the FFmpeg project in any way.**
 
 # Usage
 
 The program arguments are as follows:
 ```
-./LongExposer input_file output_file
+./LongExposer [options] input_file output_file
 ```
 
 You can run it on the example file after compiling with the following command (from the project root):
@@ -22,7 +24,7 @@ You can run it on the example file after compiling with the following command (f
 ./build/LongExposure example.mp4 example.ppm
 ```
 
-The `ppm` formatted image can be converted to more friendly image formats using a thirdparty program. Here is an example using the commonly available tool FFmpeg:
+The `ppm` formatted image can be converted to more widely usable image formats using a thirdparty program. Here is an example using the commonly available tool FFmpeg:
 ```
 ffmpeg -i ppm_file.ppm jpeg_file.jpg
 ```
@@ -31,4 +33,18 @@ The program primarily works for short videos where the camera taking the video i
 ```
 ffmpeg -i input_file.mp4 -vf vidstabdetect=tripod=1 -f null -
 ffmpeg -i input_file.mp4 -vf vidstabtransform=tripod=1 stabilized.mp4
+```
+
+### Additional command-line options
+**--mode \(short form: -m\) \[default value: average\]:** Set the mode of the program. The average mode is the one I have used most, but the others can create some very interesting effects. The following are possible modes:
+ - **average** [default] - Average all of the frames together by averaging each of the RGB color components of each pixel across all frames. Useful for creating interesting "long exposure" types of effects with water.
+ - **brightest** - Uses the highest value for each color component for each pixel across the whole video (i.e., uses the highest R component of a pixel across all frames, the highest G component across all frames, and the highest B component across all frames). This can also create some interesting effects (note that this does not use the brightest value for the entire _pixel_, but the brightest value for each _component_ of each pixel).
+ - **sum** - sums all of the R, G, and B components for each pixel across all frames (capping the value at 255).
+ - **sum-scaled** - sums all of the R, G, and B components for each pixel across all frames, but rather than capping each component individually, the highest component of a pixel is capped at 255 and the lower components are scaled equivalently (in other words, each component sum of a pixel is multiplied by a "scale value" equal to 255/highest_component_sum_value).
+
+**--intensity \(short form: -i\) \[default value: 1.0\]:** A decimal value between 0.0 and 1.0 (inclusive) used to scale the intensity of the image. For "brightest" mode, each component of each pixel is multiplied by this value. For other modes, each sum for each component of each pixel is multiplied by this value _before_ any other operations (capping/scaling/averaging the pixels). For example, using the "sum" mode with an intensity of (1/video_frame_count) is the equivalent of the "average" mode.
+
+The following example applies an intensity of 0.8 in sum mode:
+```
+./LongExposure -i 0.8 -m sum input.mp4 output.ppm
 ```
--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@
 #include "FrameExtractor.hpp"
 #include "AVFrameHolder.hpp"
 #include "RGBFrameConverter.hpp"
+#include "thirdparty/argparse/argparse.hpp"
 extern "C"
 {
 #include <libavcodec/avcodec.h>
@@ -39,6 +40,11 @@ extern "C"
 using std::string;
 using std::unique_ptr;
 using std::vector;
+
+static const string MODE_AVERAGE = "average";
+static const string MODE_BRIGHTEST = "brightest";
+static const string MODE_SUM = "sum";
+static const string MODE_SUM_SCALED = "sum-scaled";
 
 static void saveImage(uint8_t* imgBytes, const string& filename, int width, int height)
 {
@@ -60,8 +66,17 @@ static void saveImage(uint8_t* imgBytes, const string& filename, int width, int 
 }
 
 // Returns an RGB frame
-AVFrameHolder long_expose_image(string filename)
+AVFrameHolder long_expose_image(string filename, const string mode, const double intensity)
 {
+    if (mode != MODE_AVERAGE && mode != MODE_SUM && mode != MODE_SUM_SCALED && mode != MODE_BRIGHTEST)
+    {
+        throw std::invalid_argument("The mode argument has an invalid value of " + mode);
+    }
+    if (intensity < 0 || intensity > 1)
+    {
+        throw std::invalid_argument("Argument `intensity` must be between 0.0 and 1.0");
+    }
+
     FrameExtractor extractor(filename);
     RGBFrameConverter rgbConvert;
     int width = extractor.GetFrameWidth();
@@ -73,6 +88,7 @@ AVFrameHolder long_expose_image(string filename)
 
     int frameCount = 0;
     AVFrameHolder frameHolder;
+    bool summing = mode != MODE_BRIGHTEST;
 
     while (extractor.NextFrame(frameHolder) >= 0)
     {
@@ -81,9 +97,19 @@ AVFrameHolder long_expose_image(string filename)
         for (int i = 0; i < width * height; i++)
         {
             int bytePos = i * 3;
-            rsums[i] += rgbBytes[bytePos];
-            gsums[i] += rgbBytes[bytePos + 1];
-            bsums[i] += rgbBytes[bytePos + 2];
+
+            if (summing)
+            {
+                rsums[i] += rgbBytes[bytePos];
+                gsums[i] += rgbBytes[bytePos + 1];
+                bsums[i] += rgbBytes[bytePos + 2];
+            }
+            else
+            {
+                rsums[i] = std::max(rgbBytes[bytePos], (uint8_t)rsums[i]);
+                gsums[i] = std::max(rgbBytes[bytePos + 1], (uint8_t)gsums[i]);
+                bsums[i] = std::max(rgbBytes[bytePos + 2], (uint8_t)bsums[i]);
+            }
         }
         frameCount++;
     }
@@ -97,9 +123,42 @@ AVFrameHolder long_expose_image(string filename)
     for (int i = 0; i < width * height; i++)
     {
         int bytePos = i * 3;
-        imgBytes[bytePos] = rsums[i] / frameCount;
-        imgBytes[bytePos + 1] = gsums[i] / frameCount;
-        imgBytes[bytePos + 2] = bsums[i] / frameCount;
+
+        // Scale the values before using them
+        rsums[i] = (uint64_t)std::round(rsums[i] * intensity);
+        gsums[i] = (uint64_t)std::round(gsums[i] * intensity);
+        bsums[i] = (uint64_t)std::round(bsums[i] * intensity);
+
+        if (mode == MODE_AVERAGE)
+        {
+            imgBytes[bytePos] = rsums[i] / frameCount;
+            imgBytes[bytePos + 1] = gsums[i] / frameCount;
+            imgBytes[bytePos + 2] = bsums[i] / frameCount;
+        }
+        else if (mode == MODE_SUM)
+        {
+            imgBytes[bytePos] = std::min(rsums[i], (uint64_t)255);
+            imgBytes[bytePos + 1] = std::min(gsums[i], (uint64_t)255);
+            imgBytes[bytePos + 2] = std::min(bsums[i], (uint64_t)255);
+        }
+        else if (mode == MODE_SUM_SCALED)
+        {
+            double scaleVal = 255.0 / std::max({rsums[i], gsums[i], bsums[i]});
+            if (scaleVal > 1)
+            {
+                // None of the components are greater than 255
+                scaleVal = 1;
+            }
+            imgBytes[bytePos] = (uint8_t)std::round(rsums[i] * scaleVal);
+            imgBytes[bytePos + 1] = (uint8_t)std::round(gsums[i] * scaleVal);
+            imgBytes[bytePos + 2] = (uint8_t)std::round(bsums[i] * scaleVal);
+        }
+        else if (mode == MODE_BRIGHTEST)
+        {
+            imgBytes[bytePos] = (uint8_t)rsums[i];
+            imgBytes[bytePos + 1] = (uint8_t)gsums[i];
+            imgBytes[bytePos + 2] = (uint8_t)bsums[i];
+        }
     }
 
     return outFrame;
@@ -107,18 +166,81 @@ AVFrameHolder long_expose_image(string filename)
 
 int main(int argc, char** argv)
 {
-    if (argc < 3)
+    static const string INPUT_FILE_ARG = "input_file";
+    static const string OUTPUT_FILE_ARG = "output_file";
+    static const string MODE_ARG = "--mode";
+    static const string INTENSITY_ARG = "--intensity";
+
+    argparse::ArgumentParser program("LongExposer");
+    program.add_argument(INPUT_FILE_ARG)
+        .help("The name of the video file to take as an input.");
+    program.add_argument(OUTPUT_FILE_ARG)
+        .help("The name to give to the generated output PPM file.");
+
+    program.add_argument(MODE_ARG, "-m")
+        .help("The mode for the program to operate in. See Readme.md for more details.")
+        .default_value(MODE_AVERAGE)
+        .action([](const string& value)
+                {
+                    static const vector<string> choices = {MODE_AVERAGE, MODE_BRIGHTEST,
+                                                           MODE_SUM, MODE_SUM_SCALED};
+                    if (std::find(choices.begin(), choices.end(), value) != choices.end())
+                    {
+                        return value;
+                    }
+                    throw std::runtime_error("Invalid value for '" + MODE_ARG + "' argument: " + value);
+                });
+
+    program.add_argument(INTENSITY_ARG, "-i")
+        .help("Scale the intensity of the image, applied before mode operations. See Readme for how this affects different modes.")
+        .default_value(1.0)
+        .action([](const string& value)
+                {
+                    double num;
+                    bool failed = false;
+                    try
+                    {
+                        num = std::stod(value);
+                    }
+                    catch (const std::invalid_argument&)
+                    {
+                        failed = true;
+                    }
+                    catch (const std::out_of_range&)
+                    {
+                        failed = true;
+                    }
+
+                    if (num < 0 || num > 1)
+                    {
+                        failed = true;
+                    }
+
+                    if (failed)
+                    {
+                        throw std::runtime_error("Argument '" + INTENSITY_ARG + "' must be a number between 0.0 and 1.0");
+                    }
+
+                    return num;
+                });
+
+    try
     {
-        std::cerr << "Error: you must specify a file to squishify and an output name.\n\n"
-                  << "Program arguments:\n"
-                  << argv[0] << " input_file output_file" << std::endl;
-        return -1;
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err)
+    {
+        std::cout << err.what() << "\n"
+                  << program;
+        return 0;
     }
 
-    string filename(argv[1]);
-    string outputName(argv[2]);
+    string filename = program.get<string>(INPUT_FILE_ARG);
+    string outputName = program.get<string>(OUTPUT_FILE_ARG);
+    string mode = program.get<string>(MODE_ARG);
+    double intensity = program.get<double>(INTENSITY_ARG);
 
-    AVFrameHolder longExposure = long_expose_image(filename);
+    AVFrameHolder longExposure = long_expose_image(filename, mode, intensity);
     uint8_t* imgBytes = longExposure.GetPointer()->data[0];
 
     saveImage(imgBytes, outputName, longExposure.GetWidth(), longExposure.GetHeight());
